6bf0b41b0621ba5be951b0b93dbfed0735a70ecf
Rafael Santiago <contact@rafaelsantiago.eu>
2020-10-25 23:36:45 +0000

    Adding timetable labels and message.

--- app/src/test/java/eu/rafaelsantiago/rimluas/MainViewModelUnitTest.kt
+++ app/src/test/java/eu/rafaelsantiago/rimluas/MainViewModelUnitTest.kt
@@ -6,12 +6,6 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.time.LocalTime
 
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class MainViewModelUnitTest {
 
     //region evaluateTime()
