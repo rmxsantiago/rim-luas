7603e701da365bf1f12c900d02bb9fc43391b88f
Rafael Santiago <contact@rafaelsantiago.eu>
2020-10-25 16:39:29 +0000

    Create network calls to Luas API (#9)
    - Creating a basic architecture;
    - Creating network calls;
    - Removing unnecessary views from the project;

---
+++ app/src/main/java/eu/rafaelsantiago/rimluas/domain/enum/LuasStopEnum.kt
@@ -0,0 +1,6 @@
+package eu.rafaelsantiago.rimluas.domain.enum
+
+enum class LuasStopEnum(val abbrev: String) {
+    MARLBOROUGH("MAR"),
+    STILLORGAN("STI")
+}
---
+++ app/src/main/java/eu/rafaelsantiago/rimluas/domain/model/Direction.kt
@@ -0,0 +1,17 @@
+package eu.rafaelsantiago.rimluas.domain.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import org.simpleframework.xml.Attribute
+import org.simpleframework.xml.ElementList
+import org.simpleframework.xml.Root
+
+@Root(name = "direction")
+@Parcelize
+data class Direction (
+    @field: Attribute(name = "name")
+    var name : String = "",
+
+    @field : ElementList(name="tram", inline = true)
+    var trams : List<Tram> = ArrayList()
+) : Parcelable
