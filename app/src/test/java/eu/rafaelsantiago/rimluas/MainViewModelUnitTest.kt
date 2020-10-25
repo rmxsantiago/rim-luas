3121a07415673015b01e4e88164377d4d748672e
Rafael Santiago <contact@rafaelsantiago.eu>
2020-10-25 21:57:27 +0000

    Criteria to show the correct tram forecast (#12)
    - Creating functions to retrieve the correct tram list based on time and direction in MainViewModel;

---
+++ app/src/test/java/eu/rafaelsantiago/rimluas/MainViewModelUnitTest.kt
@@ -0,0 +1,103 @@
+package eu.rafaelsantiago.rimluas
+
+import eu.rafaelsantiago.rimluas.domain.enum.LuasStopEnum
+import eu.rafaelsantiago.rimluas.presentation.main.viewmodel.MainViewModel
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.time.LocalTime
+
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class MainViewModelUnitTest {
+
+    //region evaluateTime()
+    @Test
+    fun `Opening app at 00_00`() {
+        // GIVEN
+        val viewmodel = MainViewModel()
+        val localTime = LocalTime.of(0, 0, 0)
+
+        // WHEN
+        val result = viewmodel.evaluateTime(localTime)
+
+        // THEN
+        val expected = LuasStopEnum.MARLBOROUGH
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `Opening app at 12_00`() {
+        // GIVEN
+        val viewmodel = MainViewModel()
+        val localTime = LocalTime.of( 12, 0, 0)
+
+        // WHEN
+        val result = viewmodel.evaluateTime(localTime)
+
+        // THEN
+        val expected = LuasStopEnum.MARLBOROUGH
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `Opening app at 12_01`() {
+        // GIVEN
+        val viewmodel = MainViewModel()
+        val localTime = LocalTime.of( 12, 1, 0)
+
+        // WHEN
+        val result = viewmodel.evaluateTime(localTime)
+
+        // THEN
+        val expected = LuasStopEnum.STILLORGAN
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `Opening app at 23_59`() {
+        // GIVEN
+        val viewmodel = MainViewModel()
+        val localTime = LocalTime.of(23, 59, 0)
+
+        // WHEN
+        val result = viewmodel.evaluateTime(localTime)
+
+        // THEN
+        val expected = LuasStopEnum.STILLORGAN
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `Opening app at 03_00`() {
+        // GIVEN
+        val viewmodel = MainViewModel()
+        val localTime = LocalTime.of(3, 0, 0)
+
+        // WHEN
+        val result = viewmodel.evaluateTime(localTime)
+
+        // THEN
+        val expected = LuasStopEnum.MARLBOROUGH
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `Opening app at 14_00`() {
+        // GIVEN
+        val viewmodel = MainViewModel()
+        val localTime = LocalTime.of(14, 0, 0)
+
+        // WHEN
+        val result = viewmodel.evaluateTime(localTime)
+
+        // THEN
+        val expected = LuasStopEnum.STILLORGAN
+        assertEquals(expected, result)
+    }
+    //endregion
+
+}
